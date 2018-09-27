363866f3c74fd8d773c9041466366f1f987c1962
priyanka <42583467+ranadep14@users.noreply.github.com>
2018-09-27 12:30:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ console.log("Hello World");
 console.log("Hello World.....................");
 console.log("This is latest file");
 console.log("This is very latest file");
+console.log("Byeeeeeeeeeeeeeeee");
