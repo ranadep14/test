c351080b15d58b0e392b1559c69325dffa1399a3
aa <a.a@nciportal.com>
2018-10-04 14:22:44 +0530

    changes in main.js

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 console.log("Hello World");
-console.log("Hello World.....................");
-console.log("This is latest file");
-console.log("This is very latest file");
-console.log("Byeeeeeeeeeeeeeeee");
-console.log("Byeeeeeeeeeeeeeeee");
-console.log("Byeeeeeeeeeeeeeeee");
-console.log("Done..........");
-console.log("Done..........");
-console.log("Done");
 console.log("Done");
